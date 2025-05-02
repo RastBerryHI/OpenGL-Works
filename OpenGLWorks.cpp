58cffcddb76330ce6dfd6dd62e287db6943b6f46
Pasha Musaiev <pmusaiev@qualium-systems.com>
2025-05-02 22:06:57 +0300

    Added buffer swapping

--- OpenGLWorks.cpp
+++ OpenGLWorks.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>
 
-#include "GL/freeglut.h"
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 
@@ -11,34 +11,33 @@ int HEIGHT = 720;
 int main(int argc, char* argv[])
 {
     glfwInit();
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Window", nullptr, nullptr);
-    
-    if (window == nullptr) {
-        std::cerr << "Failed to create GLFW Window\n";
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Works", nullptr, nullptr);
+
+    if (!window) {
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
 
-   /* glewExperimental = GL_TRUE;
-
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW\n";
-        return -1;
-    }*/
+    gladLoadGL();
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        glfwSwapBuffers(window);
     }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
