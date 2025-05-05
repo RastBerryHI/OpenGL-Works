e90881f151c5a5444af50bef5129cbce1b135bb7
Combine-17 <56451598+RastBerryHI@users.noreply.github.com>
2025-05-05 19:34:20 +0300

    Fps camera (#1)
    * Added window resize callback
    
    * Added diffuse lightning
    
    * Added ambient lightning controll
    
    * Added specular light
    
    * Added fps camera

--- Camera.h
+++ Camera.h
@@ -17,6 +17,7 @@ public:
 	glm::vec3 Position;
 	glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::mat4 cameraMatrix = glm::mat4(1.0f);
 
 	bool firstClick = true;
 
@@ -28,8 +29,10 @@ public:
 
 	Camera(int width, int height, glm::vec3 position);
 
-	void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform);
-	void Inputs(GLFWwindow* window);
+	void UpdateMatrix(float FOVdeg, float nearPlane, float farPlane);
+	void Matrix(Shader& shader, const char* uniform);
+	void Inputs(GLFWwindow* window, bool isFps);
+	void UpdateWindowProps(GLFWwindow* window, int width, int height);
 };
 
 #endif
